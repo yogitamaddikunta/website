12757bd35036a9b9979ad04ab044e916b11a6a40
yogitamaddikunta <yogitamaddikunta@gmail.com>
2023-12-17 17:56:00 +0530

    changes

---
+++ dist/Grocery.f7a83773.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire164e;(0,e.register)("iO0e0",function(r,i){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var a=e("ayMG0");e("acw62");var l=()=>(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Grocery ARE AVAILABLE HERE "})})});
+//# sourceMappingURL=Grocery.f7a83773.js.map
